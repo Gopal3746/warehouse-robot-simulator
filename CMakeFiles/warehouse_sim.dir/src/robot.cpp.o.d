666a2d57c7859f35733893dc982f858be7c8fac0
Gopal3746 <gopalkamineni2001@gmail.com>
2026-08-29 06:51:33 -0400

    initialize warehouse robot simulator

---
+++ CMakeFiles/warehouse_sim.dir/src/robot.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/warehouse_sim.dir/src/robot.cpp.o: \
+  /Users/gopalkamineni/Downloads/warehouse-robot-simulator/src/robot.cpp \
+  /Users/gopalkamineni/Downloads/warehouse-robot-simulator/include/robot.hpp \
+  /Users/gopalkamineni/Downloads/warehouse-robot-simulator/include/position.hpp
